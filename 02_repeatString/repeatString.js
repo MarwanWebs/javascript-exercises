f0e5c6409742f3caa0d1c7d1c43569cbf883cc6d
marwanwebs <libyandoesgt@gmail.com>
2023-06-17 22:12:20 +0200

    finished second section!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newstring = ''
+    if (num > 0) {
+        for(i = 0; i < num; i++) {
+         newstring += string
+            
+        }
+        return newstring
+    }
+    else if (num === 0){
+        return ''
+    }
+    else if (num < 0) {
+        return 'ERROR'
+    }
 
 };
 
