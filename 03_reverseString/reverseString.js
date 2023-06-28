b1ca7a2348a36cdd29876f5720157e95b9b3e8d8
marwanwebs <libyandoesgt@gmail.com>
2023-06-28 23:47:09 +0200

    finished the third exercise LETS GOOOOOOOOOOOOOO

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    splitString = string.split('')
+    let reversed = ''
+    for(i = splitString.length - 1; i >= 0; i--){
+        reversed += splitString[i]
+    }
+    return reversed
 };
 
 // Do not edit below this line
